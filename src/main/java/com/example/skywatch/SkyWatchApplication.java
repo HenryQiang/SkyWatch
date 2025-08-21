448089268570301452a91977d0d20a6ccc1f26ec
Henry <1184100412@qq.com>
2025-08-21 21:33:32 +0800

    第二次提交

--- src/main/java/com/example/skywatch/SkyWatchApplication.java
+++ src/main/java/com/example/skywatch/SkyWatchApplication.java
@@ -8,6 +8,7 @@ public class SkyWatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkyWatchApplication.class, args);
+        System.out.println("你无敌了");
     }
 
 }
